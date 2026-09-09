3bd346f24b10c50721af91d32ec70be1efb02250
mmbbec <70773055+mmbbec@users.noreply.github.com>
2026-09-09 15:37:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 const CONFIG = {
   // ⚠️ IMPORTANT: Replace this with your Apps Script Web App URL
-  API_URL: 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbznmFhMaAOZU8MCjbrJlkymrCSsv24-F2AUBFIvX7k7jBuxRoIQiGziangK1iG6mjGxaQ/exec',
   
   APP_NAME: 'Subject Teacher Dashboard',
   SESSION_SECONDS: 21600
