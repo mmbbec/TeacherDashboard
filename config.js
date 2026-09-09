d80f03d6ac3ac1093d852686ada6e3abd04596e1
mmbbec <70773055+mmbbec@users.noreply.github.com>
2026-09-09 15:50:15 +0530

    Update API_URL to new Web App URL

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 const CONFIG = {
   // ⚠️ IMPORTANT: Replace this with your Apps Script Web App URL
-  API_URL: 'https://docs.google.com/spreadsheets/d/1BDH-vTQ-sWrdptF6pTWQ8FtnRNnd1Nw22UymxY0uJSc/edit?usp=sharing',
+  API_URL: 'https://script.google.com/macros/s/AKfycbx6CBes1QgpimZMZW1taLxAPtuvJ8K7vRvpV4fll4KAxYyv_0SPWRMU7Kokxuiiwv6Txg/exec',
   
   APP_NAME: 'Subject Teacher Dashboard',
   SESSION_SECONDS: 21600
